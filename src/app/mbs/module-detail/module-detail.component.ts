0a40964c99d35648f5b470e580599c2513b1a61e
mprahl <matthew.prahl@outlook.com>
2017-11-16 15:24:55 -0500

    Add a progress bar on the module build details page

--- src/app/mbs/module-detail/module-detail.component.ts
+++ src/app/mbs/module-detail/module-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 import { Observable } from 'rxjs/Observable';
@@ -12,25 +12,35 @@ import { MbsModule } from 'mbs/types/mbs.type';
   templateUrl: './module-detail.component.html',
   styleUrls: ['./module-detail.component.scss']
 })
-export class ModuleDetailComponent implements OnInit {
+export class ModuleDetailComponent implements OnInit, OnDestroy {
 
   module: MbsModule;
+  interval: any;
   constructor(private route: ActivatedRoute, private moduleService: ModuleService) { }
 
   ngOnInit() {
-    var moduleObservable: Observable<MbsModule> = this.route.paramMap.switchMap(
-      (params: ParamMap) => this.moduleService.getModule(+params.get('id')));
-    moduleObservable.subscribe(
-      data => {
-        this.module = data;
-        this.title = this.getModuleTitle()
-      },
-      error => {
-        console.log(error);
-      }
-    )
+    this.getModule()
+    // Run it every 15 seconds
+    this.interval = setInterval(() => {this.getModule();}, 15000);
   }
 
+  ngOnDestroy() {
+    clearInterval(this.interval);
+  }
+
+  getModule(): void {
+    // Don't reload the page when the build has failed or completed
+    if (this.module == null || (this.module.state_name != 'failed' && this.module.state_name != 'ready')) {
+      var moduleObservable: Observable<MbsModule> = this.route.paramMap.switchMap(
+        (params: ParamMap) => this.moduleService.getModule(+params.get('id')));
+        moduleObservable.subscribe(
+          data => {
+            this.module = data;
+          },
+          error => {
+            console.log(error);
+          }
+        )
     }
   }
 }
