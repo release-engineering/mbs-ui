3e2cc6c97fa845eb1162522f0f8904bfff0ab40d
mprahl <matthew.prahl@outlook.com>
2018-03-01 10:14:31 -0500

    Use the new PDC modules API internally

--- src/environments/environment.redhat.ts
+++ src/environments/environment.redhat.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   mbsUrl: 'https://mbs.engineering.redhat.com/module-build-service/1/',
   kojiUrl: 'https://brewweb.engineering.redhat.com/brew/',
-  pdcApiUrl: 'https://pdc.engineering.redhat.com/rest_api/v1/unreleasedvariants/'
+  pdcApiUrl: 'https://pdc.engineering.redhat.com/rest_api/v1/modules/'
 };
